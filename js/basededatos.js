b094e00a566fdb62d8b9bce34b72d62f86a5693d
piperno <juan.piperno@gmail.com>
2022-06-29 23:42:03 -0300

    v4

--- js/basededatos.js
+++ js/basededatos.js
@@ -1,18 +1,19 @@
-const tipoVivienda = [{"tipo": "Casa", "factor": 1.05},
-                      {"tipo": "P.H.", "factor": 1.01},
-                      {"tipo": "Edificio Departamento", "factor": 1.00},
-                      {"tipo": "Barrio Privado", "factor": 1.19},
-                      {"tipo": "Local Comercial", "factor": 2.91},
-                      {"tipo": "Depósito - Logística", "factor": 6.95},
-                      {"tipo": "Oficina Profesional", "factor": 3.33}]
+const tipoDestino = [
+    {"llega": "Bariloche", "factor": 1.05},
+    {"llega": "Mendoza", "factor": 1.01},
+    {"llega": "Córdoba", "factor": 1.00},
+    {"llega": "Ushuaia", "factor": 1.19},
+    {"llega": "Salta", "factor": 2.91},
+    {"llega": "Iguazú", "factor": 6.95},
+    {"llega": "Calafate", "factor": 3.33}
+]    
 
-const zonaVivienda = [{"area": "CABA", "factor": 1.02}, 
-                      {"area": "La Plata", "factor": 1.03},
-                      {"area": "Tandil", "factor": 0.92},
-                      {"area": "San Luis", "factor": 0.79},
-                      {"area": "Costa Atlántica", "factor": 1.19},
-                      {"area": "Santiago de Chile", "factor": 2.03},
-                      {"area": "Lima, Perú", "factor": 1.89}]
-
-
-const costoSeguroM2 = 29.75
+const tipoPartida = [
+    {"sale": "Ciudad de Buenos Aires", "factor": 1.02}, 
+    {"sale": "Córdoba Capital", "factor": 1.03},
+    {"sale": "Rosario", "factor": 0.92},
+    {"sale": "Ciudad de Mendoza", "factor": 0.79},
+    {"sale": "Mar del Plata", "factor": 1.19},
+    {"sale": "Montevideo", "factor": 2.03},
+    {"sale": "Rio", "factor": 1.89}
+]
