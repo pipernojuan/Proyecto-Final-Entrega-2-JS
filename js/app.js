e8058a22667427be4bfc8fa044925c4f60a6928a
piperno <juan.piperno@gmail.com>
2022-06-30 09:20:53 -0300

    v23

--- js/app.js
+++ js/app.js
@@ -4,3 +4,5 @@ selectTipoDestino.innerHTML = cargoDestino()
 btnCotizar.addEventListener("click", ()=> cotizarVuelo() )
 
 btnRefrescar.addEventListener("click", ()=> location.reload() )
+
+localStorage.setItem (cargoPartida, cargoPartida, cotizarVuelo)
