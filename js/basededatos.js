75b18d955a5903c27940ea62b78c29545eea7bfd
piperno <juan.piperno@gmail.com>
2022-06-30 02:21:55 -0300

    v16

--- js/basededatos.js
+++ js/basededatos.js
@@ -1,10 +1,10 @@
 const tipoDestino = [
-    {"llega": "Bariloche", "factor": 1.35},
-    {"llega": "Mendoza", "factor": 2.01},
-    {"llega": "Córdoba", "factor": 3.00},
+    {"llega": "Bariloche", "factor": 4.35},
+    {"llega": "Mendoza", "factor": 3.01},
+    {"llega": "Córdoba", "factor": 1.00},
     {"llega": "Ushuaia", "factor": 9.19},
-    {"llega": "Salta", "factor": 5.91},
-    {"llega": "Iguazú", "factor": 6.95},
+    {"llega": "Salta", "factor": 3.91},
+    {"llega": "Iguazú", "factor": 5.95},
     {"llega": "Calafate", "factor": 7.33}
 ]    
 
@@ -12,10 +12,10 @@ const tipoPartida = [
     {"sale": "Ciudad de Buenos Aires", "factor": 2.02}, 
     {"sale": "Córdoba Capital", "factor": 2.03},
     {"sale": "Rosario", "factor": 3.92},
-    {"sale": "Ciudad de Mendoza", "factor": 0.14},
-    {"sale": "Mar del Plata", "factor": 5.19},
+    {"sale": "Ciudad de Mendoza", "factor": 4.14},
+    {"sale": "Mar del Plata", "factor": 2.19},
     {"sale": "Montevideo", "factor": 6.03},
     {"sale": "Rio", "factor": 8.89}
 ]
 
-const costoPorPersona = 29.75
+const costoPorPersona = 15000.35
